d3dacca3e890234c06c30962d5fbcf60d123cc64
Juan Montoya <juanmontoya940@gmail.com>
2024-05-09 20:27:49 -0500

    added logout button

--- app/(app)/profile.jsx
+++ app/(app)/profile.jsx
@@ -1,11 +1,24 @@
 import {View, Text} from "react-native";
-import React from "react";
+import React, {useState} from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+import CustomButton from "../../components/custombutton";
+import * as SecureStore from "expo-secure-store";
+import {useRouter} from "expo-router";
+
 
 const Profile = () => {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+    const logout = async () => {
+        setLoading(true);
+        await SecureStore.deleteItemAsync("Token");
+        setLoading(false)
+        router.replace("/");
+    }
     return (
-        <View>
-            <Text>Profile</Text>
-        </View>
+        <SafeAreaView className={"bg-primary h-full justify-center px-4"}>
+            <CustomButton isLoading={loading} title={"Logout"} handlePress={()=>logout()} />
+        </SafeAreaView>
     );
 };
 export default Profile;
